c217bb063b3874b7d0197ad3a574b69516647325
mr-chenguang <37072324+lcgash@users.noreply.github.com>
2021-05-09 08:17:02 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,34 +2,6 @@
 
 set -e
 
-# check values
-
-if [ -n "${PUBLISH_REPOSITORY}" ]; then
-    TARGET_REPOSITORY=${PUBLISH_REPOSITORY}
-else
-    TARGET_REPOSITORY=${GITHUB_REPOSITORY}
-fi
-
-if [ -n "${BRANCH}" ]; then
-    TARGET_BRANCH=${BRANCH}
-else
-    TARGET_BRANCH="gh-pages"
-fi
-
-if [ -n "${PUBLISH_DIR}" ]; then
-    TARGET_PUBLISH_DIR=${PUBLISH_DIR}
-else
-    TARGET_PUBLISH_DIR="./public"
-fi
-
-if [ -z "$PERSONAL_TOKEN" ]
-then
-  echo "You must provide the action with either a Personal Access Token or the GitHub Token secret in order to deploy."
-  exit 1
-fi
-
-REPOSITORY_PATH="https://x-access-token:${PERSONAL_TOKEN}@github.com/${TARGET_REPOSITORY}.git"
-
 # deploy to
 echo "Deploy to ${TARGET_REPOSITORY}"
 
@@ -53,7 +25,6 @@ ls -l /root/.ssh/
 echo "npm install ..."
 npm install
 
-
 echo "Clean folder ..."
 ./node_modules/hexo/bin/hexo clean
 
@@ -61,7 +32,7 @@ echo "Generate file ..."
 ./node_modules/hexo/bin/hexo generate
 
 echo "Deploy file ..."
-echo "${GITHUB_ACTOR}"
 git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global user.email "${GITHUB_ACTOR}@163.com"
+
 ./node_modules/hexo/bin/hexo deploy
